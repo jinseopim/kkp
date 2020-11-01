63f1212fe3d21e688e15067b57ee1045abe08cc1
jinseopim <zenxzen@naver.com>
2020-11-01 16:50:06 +0900

    summary 3 commit

--- summary/models.py
+++ summary/models.py
@@ -5,14 +5,13 @@ from django.utils import timezone
 
 class Summary(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    section_name = models.CharField(max_length=1000, null=True)
+    category_name = models.CharField(max_length=1000, null=True)
+    summary_name = models.TextField(null=True)
+    url = models.CharField(max_length=4000, null=True)
     date = models.DateTimeField()
     thumbnail = models.ImageField(u'썸네일',
                         upload_to='%Y/%m/%d', blank=True, null=True)
-    join_users = models.ManyToManyField('auth.User',
-                        verbose_name=u'참석', blank=True,
-                        related_name='join_moim')
     created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
