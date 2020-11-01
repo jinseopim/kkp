bde6276fc53c0cb52c1796accec9453fafe27359
jinseopim <zenxzen@naver.com>
2020-11-01 16:53:45 +0900

    summary 4 commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -70,7 +70,7 @@ print("BASE_DIR : ", BASE_DIR)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR) + '/templates/',],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
